b8a74edcaa7b9705d0de379fcfd8bd3dd555ca3a
Nagaraju112333 <nagaraju.boda@archents.com>
2023-02-15 11:33:03 +0530

    ecommarc3e

---
+++ ArchentsFirstProject/obj/Release/Package/PackageTmp/Scripts/Product/Cart.js
@@ -0,0 +1,22 @@
+﻿
+function Addtocart(item) {
+    var itemId = $(item).attr("itemid")
+    var formdata = new FormData();
+    formdata.append("ItemId", itemId);
+    $.ajax({
+        async: true,
+        type: "Post",
+        contentType: false,
+        processData: false,
+        data: formdata,
+        url: "/Products/ProductPost/",
+        success: function (data) {
+            if (data.success) {
+                $("#Carditgem").text("cart(" + data.Counter + ")");
+            }
+        },
+        error: function () {
+            alert("there is something is Wrong....")
+        }
+    });
+}
---
+++ ArchentsFirstProject/obj/Release/Package/PackageTmp/Scripts/Product/Projectsize.js
@@ -0,0 +1,10 @@
+﻿const SizeBtn = document.querySelectorAll('.size-radio-btn');
+let checkBtn = 0;
+SizeBtn.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        SizeBtn[checkBtn].classList.remove('check');
+        item.classList.add('check');
+        checkBtn = i;
+    })
+})
+document.getElementById("size").innerHTML = value();
